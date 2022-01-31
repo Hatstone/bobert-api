70d8a9c42f8f727705051940ed0e2c863d456836
Deuce Black <56012328+VitaminTussle@users.noreply.github.com>
2022-01-31 14:06:14 -0600

    Boiler plate Spring Boot project

---
+++ src/main/java/com/hatstone/bobertapi/BobertApiApplication.java
@@ -0,0 +1,13 @@
+package com.hatstone.bobertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hatstone/bobertapi/BobertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hatstone.bobertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
